1230bb9048cc42f0a612bb626341aa5c8654075f
nonameebyrator985 <singheingemo@hotmail.com>
2025-09-30 23:42:24 +0900

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const axios = require('axios');
+
+// Fonction principale de l'outil
+const main = () => {
+    console.log('Outil de Migration Legacy démarré...');
+    // Logic de migration ici
+};
+
+main();
